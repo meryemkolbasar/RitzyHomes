c72251451a8212d40c0b9f8bf7542a7cc3394ffa
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2024-01-07 04:09:51 +0100

    Login gelisim asamasinda.

--- src/components/login/login-form.jsx
+++ src/components/login/login-form.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from 'react-router-dom';
+import { Col, Row } from 'react-bootstrap';
+import './login-form.scss';
 
 const LoginForm = () => {
-  return (
-    <div>LoginForm</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default LoginForm
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Burada gerçek bir kimlik doğrulama işlemi gerçekleştirilecek.
+    // Örneğin, bir API'ye e-posta ve şifreyi gönderip kontrol edilebilir.
+    // Eğer giriş başarılı ise, kullanıcıyı yönlendir
+
+    navigate('/home/content');
+  };
+
+  return (
+    <Row className="login-container">
+      <Col md={2}>
+        <form>
+          <label>
+            Email
+            <input className="email-input" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </label>
+          <br />
+          <label>
+            Password
+            <input className="password-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+          <br />
+          <Link className="forgot-password-link" style={{ textDecoration: "none", color: 'black' }}> Forgot password?</Link>
+          <br />
+          <button className="login-button" type="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+        <p className="register-now-paragraph">
+          If you don't have an account,{" "}
+          <Link to="/user/register" className="register-now-link">Register now!</Link>
+        </p>
+      </Col>
+    </Row>
+  );
+};
+
+export default LoginForm;
