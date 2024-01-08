5aec7e882b029f497413df62b60403b99478ae70
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2024-01-08 05:25:12 +0100

    edit-advert-page olusturuldu & gelistiriliyor. CSS kod blogu normalize edildi & renkler import edildi.

---
+++ src/components/edit-advert/edit-advert.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './edit-advert.scss'
+
+const EditAdvert = () => {
+  return (
+    <> 
+
+    </>
+  )
+}
+
+export default EditAdvert
---
+++ src/pages/edit-advert-page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageHeader from "../components/common/page-header";
+import EditAdvert from "../components/edit-advert/edit-advert";
+
+const EditAdvertPage = () => {
+  return (
+    <>
+      <PageHeader title="EDIT ADVERT" />
+      <EditAdvert />
+    </>
+  );
+};
+
+export default EditAdvertPage;
