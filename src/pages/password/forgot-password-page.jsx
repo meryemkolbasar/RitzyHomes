d249e269fffcab18730da84ea11e88809549bcb8
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2024-01-12 18:07:35 +0100

    Forgot Password Page tamamlandi.

---
+++ src/pages/password/forgot-password-page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../../components/common/page-header'
+import ForgotPassword from '../../components/password/forgot-password'
+
+const ForgotPasswordPage = () => {
+  return (
+    <>
+    <PageHeader title= 'FORGOT PASSWORD'/>
+    <ForgotPassword />
+    </>
+  )
+}
+
+export default ForgotPasswordPage
