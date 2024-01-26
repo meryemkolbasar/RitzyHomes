0bfad9214b5ce9b2180ba429660fde0bac3ebdef
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2024-01-26 18:50:28 +0100

    -u

--- src/components/advert/add-new-advert.jsx
+++ src/components/advert/add-new-advert.jsx
@@ -28,39 +28,88 @@ const AddNewAdvert = () => {
   });
 
   const [images, setImages] = useState([]);
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  const handleToggleSelect = (index) => {
+    if (selectedImages.includes(index)) {
+      setSelectedImages((prevSelected) =>
+        prevSelected.filter((i) => i !== index)
+      );
+    } else {
+      setSelectedImages((prevSelected) => [...prevSelected, index]);
+    }
+  };
+
+  const handleSetAsFeatured = () => {
+    if (selectedImages.length > 0) {
+      const featuredIndex = selectedImages[0];
+      console.log("Set as Featured:", featuredIndex);
+    } else {
+      console.log("No image selected to set as featured");
+    }
+  };
+
+  const handleImageDelete = () => {
+    if (selectedImages.length > 0) {
+      const remainingImages = images.filter((_, index) => !selectedImages.includes(index));
+      setImages(remainingImages);
+      setSelectedImages([]);
+    } else {
+      console.log("No image selected to delete");
+    }
+  };
 
   const handleImageAdd = () => {
     // Burada resim ekleme fonksiyonu implement edilebilir.
     // Örneğin, kullanıcı bir dosya seçtiğinde bu dosya state'e eklenir.
   };
 
-  const handleSetAsFeatured = (index) => {
-    // Burada resimler arasında öne çıkanı belirleme fonksiyonu implement edilebilir.
-    // index parametresi, hangi resmin öne çıkacağını belirtir.
-  };
-
-  const handleImageDelete = (index) => {
-    // Burada resim silme fonksiyonu implement edilebilir.
-    // index parametresi, hangi resmin silineceğini belirtir.
-  };
-
   const handleCreateAdvert = () => {
-    // Burada yeni ilan oluşturma fonksiyonu implement edilebilir.
-    // State'lerde bulunan bilgileri kullanarak ilan oluşturulabilir.
+    const newAdvert = {
+      commonInfo,
+      addressInfo,
+      properties,
+      images,
+    };
+
+    console.log("New Advert:", newAdvert);
+
+    setCommonInfo({
+      title: "",
+      description: "",
+      price: "",
+      advertType: "",
+      category: "",
+    });
+    setAddressInfo({
+      country: "",
+      city: "",
+      neighbourhood: "",
+      location: "",
+    });
+    setProperties({
+      floor: "",
+      bedroom: "",
+      bathroom: "",
+      garage: "",
+      yearOfBuilt: "",
+      size: "",
+    });
+    setImages([]);
+    setSelectedImages([]);
   };
 
   return (
     <Container>
       <Row className="add-new-advert-container">
-        {/* Common Information */}
-        <Col md={6}>
+        
+        <Col md={12}>
           <Form>
             <h5>Common Information</h5>
             <Form.Group controlId="formTitle">
               <Form.Label>Title</Form.Label>
               <Form.Control
                 type="text"
-                //placeholder="Enter title"
                 value={commonInfo.title}
                 onChange={(e) => setCommonInfo({ ...commonInfo, title: e.target.value })}
               />
@@ -71,7 +120,6 @@ const AddNewAdvert = () => {
               <Form.Control
                 as="textarea"
                 rows={3}
-               // placeholder="Enter description"
                 value={commonInfo.description}
                 onChange={(e) => setCommonInfo({ ...commonInfo, description: e.target.value })}
               />
@@ -81,7 +129,6 @@ const AddNewAdvert = () => {
               <Form.Label>Price</Form.Label>
               <Form.Control
                 type="text"
-               // placeholder="Enter price"
                 value={commonInfo.price}
                 onChange={(e) => setCommonInfo({ ...commonInfo, price: e.target.value })}
               />
@@ -109,38 +156,137 @@ const AddNewAdvert = () => {
           </Form>
         </Col>
 
-        {/* Address Information */}
-        <Col md={6}>
+        
+        <Col md={12}>
           <Form>
             <h5>Address Information</h5>
-            {/* Address information form fields */}
+            <Form.Group controlId="country">
+              <Form.Label>Country</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter country"
+                value={addressInfo.country}
+                onChange={(e) => setAddressInfo({ ...addressInfo, country: e.target.value })}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="city">
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter city"
+                value={addressInfo.city}
+                onChange={(e) => setAddressInfo({ ...addressInfo, city: e.target.value })}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="neighbourhood">
+              <Form.Label>Neighbourhood</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter neighbourhood"
+                value={addressInfo.neighbourhood}
+                onChange={(e) => setAddressInfo({ ...addressInfo, neighbourhood: e.target.value })}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="location">
+              <Form.Label>Location</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter location"
+                value={addressInfo.location}
+                onChange={(e) => setAddressInfo({ ...addressInfo, location: e.target.value })}
+              />
+            </Form.Group>
           </Form>
         </Col>
 
-        {/* Properties */}
-        <Col md={6}>
+        <Col md={12}>
           <Form>
             <h5>Properties</h5>
-            {/* Properties form fields */}
+            <Form.Group controlId="floor">
+              <Form.Label>Floor</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter floor"
+                value={properties.floor}
+                onChange={(e) => setProperties({ ...properties, floor: e.target.value })}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="bedroom">
+              <Form.Label>Bedroom</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter bedroom count"
+                value={properties.bedroom}
+                onChange={(e) => setProperties({ ...properties, bedroom: e.target.value })}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="bathroom">
+              <Form.Label>Bathroom</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter bathroom count"
+                value={properties.bathroom}
+                onChange={(e) => setProperties({ ...properties, bathroom: e.target.value })}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="garage">
+              <Form.Label>Garage</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter garage count"
+                value={properties.garage}
+                onChange={(e) => setProperties({ ...properties, garage: e.target.value })}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="yearOfBuilt">
+              <Form.Label>Year of Built</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter year of built"
+                value={properties.yearOfBuilt}
+                onChange={(e) => setProperties({ ...properties, yearOfBuilt: e.target.value })}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="size">
+              <Form.Label>Size</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter size"
+                value={properties.size}
+                onChange={(e) => setProperties({ ...properties, size: e.target.value })}
+              />
+            </Form.Group>
           </Form>
         </Col>
 
-        {/* Images */}
-        <Col md={6}>
+        
+        <Col md={12}>
           <div>
             <h5>Images</h5>
             {images.map((image, index) => (
-              <div key={index} className="image-container">
-                <img src={image} alt={`image-${index}`} />
+              <div key={index} className={`image-container ${selectedImages.includes(index) ? 'selected' : ''}`}>
+                <img
+                  src={image}
+                  alt={`image-${index}`}
+                  onClick={() => handleToggleSelect(index)}
+                />
                 <Button
                   variant="outline-primary"
-                  onClick={() => handleSetAsFeatured(index)}
+                  onClick={handleSetAsFeatured}
                 >
                   Set as Featured
                 </Button>
                 <Button
                   variant="outline-danger"
-                  onClick={() => handleImageDelete(index)}
+                  onClick={handleImageDelete}
                 >
                   Delete
                 </Button>
@@ -152,7 +298,7 @@ const AddNewAdvert = () => {
           </div>
         </Col>
 
-        {/* Create Button */}
+       
         <Col md={12} className="text-center">
           <Button variant="success" onClick={handleCreateAdvert}>
             Create 
