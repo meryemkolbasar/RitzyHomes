1268622d34f8b5db4361c1b81798150125585e2b
Meryem Kolbasar <kolbasarmeryem@gmail.com>
2024-01-29 11:55:11 +0100

    PageHeader eklendi -yasinibin

--- src/pages/contact-page.jsx
+++ src/pages/contact-page.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Contact from '../components/contact/contact'
 import Spacer from '../components/common/spacer'
-
+import PageHeader from '../components/common/page-header'
 import VisitOurOffice from '../components/contact/visit-our-office'
 
 const ContactPage = () => {
   return (
     <>
-    
+    <PageHeader title= 'CONTACT US'/>
     <Contact/>
     <Spacer/>
     <VisitOurOffice/>
